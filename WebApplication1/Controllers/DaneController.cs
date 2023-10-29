521533dc548605b332de4b19f94148b1a975d5f4
dominik.studnicki <dominik.studnicki@HK-02>
2023-10-29 13:19:35 +0100

    Add project files.

---
+++ WebApplication1/Controllers/DaneController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Wynik", dane);
+            }
+            else
+            {
+                return View(); // This will return the "Form" view with validation errors
+            }
+        }
+
+        public IActionResult Wynik(Dane dane)
+        {
+            return View(dane);
+        }
+    }
+}
